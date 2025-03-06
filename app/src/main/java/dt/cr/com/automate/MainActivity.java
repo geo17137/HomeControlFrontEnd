03017fc1f2696aee9d80371902ecd1cb4f50b5fd
geo17137 <supportjava.dt@gmail.com>
2025-03-06 13:18:31 +0100

    Mise à jour manifest

--- app/src/main/java/dt/cr/com/automate/MainActivity.java
+++ app/src/main/java/dt/cr/com/automate/MainActivity.java
@@ -114,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
   private static final CharSequence SSID1 = "jeanbart";
   private static final CharSequence SSID2 = "jeanbart_5GHz";
   private static final CharSequence SSID3 = "jeanbart_plus";
-  private static final CharSequence SSID4 = "AndroidWifi1";
+  private static final CharSequence SSID4 = "AndroidWifi";
 
   //-----------------------
   //    Modes de fonct. VMC
