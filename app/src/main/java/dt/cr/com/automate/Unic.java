346d31c176af567f4831e5ac3a31c912be2ed4d7
geo17137 <supportjava.dt@gmail.com>
2025-07-15 15:28:59 +0200

    Power cooking ON/OFF programming

--- app/src/main/java/dt/cr/com/automate/Unic.java
+++ app/src/main/java/dt/cr/com/automate/Unic.java
@@ -7,7 +7,7 @@ import android.widget.ImageButton;
 class Unic {
 
   @SuppressLint("StaticFieldLeak")
-  final String version = "2024.04.01";
+  final String version = "2024.07.16";
 
   private static final Unic ourInstance = new Unic();
 
