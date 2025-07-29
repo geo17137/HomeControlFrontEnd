c0ba0ae4723d2a569c366bb4165eb29da3c780e9
geo17137 <supportjava.dt@gmail.com>
2025-07-29 22:51:04 +0200

    Update Unic.java

--- app/src/main/java/dt/cr/com/automate/Unic.java
+++ app/src/main/java/dt/cr/com/automate/Unic.java
@@ -7,12 +7,7 @@ import android.widget.ImageButton;
 class Unic {
 
   @SuppressLint("StaticFieldLeak")
-<<<<<<< HEAD
   final String version = "2025.07.29";
-=======
-  final String version = "2025.07.28";
->>>>>>> master
-
   private static final Unic ourInstance = new Unic();
 
   // Position des entités dans la structure des paramètres
